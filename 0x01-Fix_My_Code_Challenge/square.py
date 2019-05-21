6f8ab3360936838514b994598b15708438f62a79
christopherchoe <chchchoe@gmail.com>
2019-05-21 00:19:08 +0000

    changed so width cannot be different from height

--- 0x01-Fix_My_Code_Challenge/square.py
+++ 0x01-Fix_My_Code_Challenge/square.py
@@ -9,13 +9,15 @@ class square():
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        if self.width != self.height:
+            self.height = self.width
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
